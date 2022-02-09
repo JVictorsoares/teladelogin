9f7910ec8bd5ec426f3a65d7dc0d7a826ec7317a
joao victor pereira soares <joaosores4@gmail.com>
2022-02-09 19:59:30 -0300

    Initial commit

---
+++ teladelogin/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace teladelogin.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
